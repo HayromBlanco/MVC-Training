d60fa44d890aafa7a6d13a86133eb7583021dcfd
Hayrom Blanco <jaruleminzy@hotmail.com>
2017-08-08 14:52:32 -0600

    First commit, doing Models

---
+++ MVC Project/MVC Project/Controllers/PlantTypeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Project.Controllers
+{
+    public class PlantTypeController : Controller
+    {
+        // GET: PlantType
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+
+    }
+}
